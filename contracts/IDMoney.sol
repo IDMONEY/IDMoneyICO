1440d762c95688de68a8092295d99b02970b9791
Eduardo Narvaez <eduardo.narvaez@sistemasgalileo.com>
2017-09-29 21:07:17 -0600

    Se agrega logica para cambiar de estado el contrato. Se agrega primera version del crowdfunding.

--- contracts/IDMoney.sol
+++ contracts/IDMoney.sol
@@ -26,6 +26,7 @@ contract IDMoney is StandardToken {
   // Se define el tipo de cambio preICO y el que tendremos durante el ICO
   uint256 public constant TOKEN_EXCHANGE_RATE_PREICO = 7000; // 7000 IDMs per 1 ETH, 30% Descuento
   uint256 public constant TOKEN_EXCHANGE_RATE_ICO = 10000; // 10000 IDMs per 1 ETH
+  uint256 public constant ETH_RECEIVED_MIN = 1; // Minima cantidad de ETH a recibir como inversion
 
   // Fundraising parameters
   enum ContractState { Larvae, PreICO, ICO, PostICO }
@@ -76,7 +77,12 @@ contract IDMoney is StandardToken {
       require(state == ContractState.PostICO);
       _;
   }
-  
+
+  modifier isFundraising() {
+      require(state == ContractState.PreICO || state == ContractState.ICO);
+      _;
+  }
+
   /**
    * @dev Permite al owner actual transferir el control a otro owner
    *      Solo se puede pasar si todavia esta en estado Larvae
@@ -86,6 +92,33 @@ contract IDMoney is StandardToken {
     if (newOwner != address(0)) {
       owner = newOwner;
     }
+  }
+
+  /**
+   * @dev Permite al owner actual ponerse en estado PreICO
+   *      Solo se puede pasar si todavia esta en estado Larvae
+   */
+  function beginPreICO() onlyOwner isLarvae {
+    // Entrar en estado PreICO
+    state = ContractState.PreICO;
+  }
+
+  /**
+   * @dev Permite al owner actual ponerse en estado ICO
+   *      Solo se puede pasar si todavia esta en estado PreICO
+   */
+  function beginICO() onlyOwner isPreICO {
+    // Entrar en estado ICO
+    state = ContractState.ICO;
+  }
+
+  /**
+   * @dev Permite al owner actual ponerse en estado PostICO
+   *      Solo se puede pasar si todavia esta en estado ICO
+   */
+  function endICO() onlyOwner isICO {
+    // Entrar en estado PostICO
+    state = ContractState.PostICO;
   }
 
   /** Eventos relacionados con la creacion de moneda IDM adicional 
@@ -103,15 +136,30 @@ contract IDMoney is StandardToken {
   }
 
   /**
+   * @dev Funcion para efectua minting de tokens para Minting y para el ICO
+   * @param _to La direccion que recibira nuevos tokens.
+   * @param _amount La cantidad de tokens a generar.
+   * @return Boolean que indica si la operacion fue exitosa.
+   */
+  function doMint(address _to, uint256 _amount) 
+  private
+  returns (bool)
+  {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
+  }
+
+  /**
    * @dev Funcion para crear nuevos tokens
    * @param _to La direccion que recibira nuevos tokens.
    * @param _amount La cantidad de tokens a generar.
    * @return Boolean que indica si la operacion fue exitosa.
    */
   function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
-    totalSupply = totalSupply.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    Mint(_to, _amount);
+    require(_amount >= 1);  // El monto a generar debe ser mayor o igual a 1
+    doMint(_to, _amount);
     return true;
   }
 
@@ -123,6 +171,36 @@ contract IDMoney is StandardToken {
     mintingFinished = true;
     MintFinished();
     return true;
+  }
+
+  /**
+   * @dev Funcion que devuelve el precio de los IDM de acuerdo a la etapa del ICO.
+   * @return El precio del token IDM por cada 1 ETH
+   */
+  function getCurrentTokenPrice() 
+    private
+    constant
+    returns (uint256 currentPrice) 
+  {
+    if (state == ContractState.PreICO) {
+        return TOKEN_EXCHANGE_RATE_PREICO;
+    } else {
+        return TOKEN_EXCHANGE_RATE_ICO;
+    }
+  }
+
+  /**
+   * @dev Funcion que revive ETH directamente pero solamente si esta en el estado correcto.
+   * @return True si la operacion fue correcta.
+   */
+  function () isFundraising payable {
+    require(msg.value > 0);  // El monto de ETH debe ser mayor que 0
+    require(msg.value > ETH_RECEIVED_MIN);  // El monto de ETH debe ser mayor que el minimo definido
+
+    // aca hay que tomar los eth (msg.value) y distribuirlos.
+    uint256 tokreceive = getCurrentTokenPrice() * msg.value; // Calcula el total de tokens a generar
+    doMint(owner,tokreceive); // Agrega tokens a billetera de dueno.
+    transferFrom(owner, msg.sender, tokreceive); // Envia tokens desde el dueno al inversor.
   }
 
 }
