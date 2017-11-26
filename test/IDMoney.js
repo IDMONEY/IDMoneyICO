6a75970ba5b22fcdde2ac7601df9223e852e72a3
Eduardo Narvaez <eduardo.narvaez@tecnicardcr.com>
2017-11-26 14:18:49 -0600

    Se agregan 3 nuevos tests.

--- test/IDMoney.js
+++ test/IDMoney.js
@@ -5,7 +5,33 @@ contract('IDMoney', function(accounts) {
     return IDMoney.deployed().then(function(instance) {
       return instance.balanceOf.call(accounts[0]);
     }).then(function(balance) {
+      //console.log('balance: '+balance.valueOf());
       assert.equal(balance.valueOf(), 0, "0 is not the balance");
     });
   });
+  it("name should be IDMONEY", function() {
+    return IDMoney.deployed().then(function(instance) {
+      return instance.name.call();
+    }).then(function(name) {
+      //console.log('name: '+name);
+      assert.equal(name, "IDMONEY", "IDMONEY is not the name");
+    });
+  });
+  it("symbol should be IDM", function() {
+    return IDMoney.deployed().then(function(instance) {
+      return instance.symbol.call();
+    }).then(function(symbol) {
+      //console.log('symbol: '+symbol);
+      assert.equal(symbol, "IDM", "IDM is not the symbol");
+    });
+  });
+  it("contract state should be 1 (notICO)", function() {
+    return IDMoney.deployed().then(function(instance) {
+      return instance.state.call();
+    }).then(function(state) {
+      //console.log('state: '+state);
+      assert.equal(state, 1, "state is not NotICO");
+    });
+  });
 });
+
